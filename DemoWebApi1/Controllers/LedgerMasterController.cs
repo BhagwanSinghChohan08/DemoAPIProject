c347dec6acf227245c60356518f814475d364834
Kamal Chauhan <bhagwansinghchohan08@gmail.com>
2025-06-15 10:21:42 +0530

    first Push

---
+++ DemoWebApi1/Controllers/LedgerMasterController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Http;
+
+[RoutePrefix("api/ledgermaster")]
+public class LedgerMasterController : ApiController
+{
+    string conStr = ConfigurationManager.ConnectionStrings["ConApplication"].ConnectionString;
+
+    // Get All Ledgers
+    [HttpGet]
+    [Route("list")]
+    public IHttpActionResult GetLedgerList()
+    {
+        return Ok(ExecuteStoredProcedure("SELECT"));
+    }
+
+    // Get Genders
+    [HttpGet]
+    [Route("gender")]
+    public IHttpActionResult GetGender()
+    {
+        return Ok(ExecuteStoredProcedure("GET_GENDER"));
+    }
+
+    // Get States
+    [HttpGet]
+    [Route("state")]
+    public IHttpActionResult GetState()
+    {
+        return Ok(ExecuteStoredProcedure("GET_STATE"));
+    }
+
+    // Get Cities by StateId
+    [HttpGet]
+    [Route("city/{stateId}")]
+    public IHttpActionResult GetCityByState(int stateId)
+    {
+        return Ok(ExecuteStoredProcedure("GET_CITY", new SqlParameter("@State_Id", stateId)));
+    }
+
+    // Insert or Update Ledger
+    [HttpPost]
+    [Route("save")]
+    public IHttpActionResult SaveLedger(LedgerModel model)
+    {
+        string action = model.Ledger_Id > 0 ? "UPDATE" : "INSERT";
+
+        List<SqlParameter> parms = new List<SqlParameter>
+    {
+        new SqlParameter("@Ledger_Id", model.Ledger_Id),
+        new SqlParameter("@Name", model.Name),
+        new SqlParameter("@Gender_Id", model.Gender_Id),
+        new SqlParameter("@ContactNo", model.ContactNo),
+        new SqlParameter("@Address1", model.Address1),
+        new SqlParameter("@Address2", model.Address2),
+        new SqlParameter("@State_Id", model.State_Id),
+        new SqlParameter("@City_Id", model.City_Id),
+        new SqlParameter("@PinCode", model.PinCode)
+    };
+
+        ExecuteStoredProcedure(action, parms);
+
+        return Ok(new { success = true });
+    }
+
+
+
+
+    //[HttpPost]
+    //[Route("save")]
+    //public IHttpActionResult SaveLedger(LedgerModel model)
+    //{
+    //    string action = model.Ledger_Id > 0 ? "UPDATE" : "INSERT";
+
+    //    List<SqlParameter> parms = new List<SqlParameter>
+    //    {
+    //        new SqlParameter("@Action", action),
+    //        new SqlParameter("@Ledger_Id", model.Ledger_Id),
+    //        new SqlParameter("@Name", model.Name),
+    //        new SqlParameter("@Gender_Id", model.Gender_Id),
+    //        new SqlParameter("@ContactNo", model.ContactNo),
+    //        new SqlParameter("@Address1", model.Address1),
+    //        new SqlParameter("@Address2", model.Address2),
+    //        new SqlParameter("@State_Id", model.State_Id),
+    //        new SqlParameter("@City_Id", model.City_Id),
+    //        new SqlParameter("@PinCode", model.PinCode)
+    //    };
+
+    //    ExecuteStoredProcedure("INSERT", parms); // insert/update both handled
+    //    return Ok(new { success = true });
+    //}
+
+    // Soft Delete
+    [HttpPost]
+    [Route("delete/{id}")]
+    public IHttpActionResult DeleteLedger(int id)
+    {
+        return Ok(ExecuteStoredProcedure("DELETE", new SqlParameter("@Ledger_Id", id)));
+    }
+
+    // Common Method to call SP
+    private DataTable ExecuteStoredProcedure(string action, params SqlParameter[] parameters)
+    {
+        DataTable dt = new DataTable();
+
+        using (SqlConnection con = new SqlConnection(conStr))
+        using (SqlCommand cmd = new SqlCommand("sp_LedgerMaster", con))
+        {
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@Action", action));
+            if (parameters != null)
+                cmd.Parameters.AddRange(parameters);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+        }
+
+        return dt;
+    }
+    //private DataTable ExecuteStoredProcedure(string action, List<SqlParameter> parms, params SqlParameter[] parameters)
+    //{
+    //    DataTable dt = new DataTable();
+
+    //    using (SqlConnection con = new SqlConnection(conStr))
+    //    using (SqlCommand cmd = new SqlCommand("sp_LedgerMaster", con))
+    //    {
+    //        cmd.CommandType = CommandType.StoredProcedure;
+    //        cmd.Parameters.Add(new SqlParameter("@Action", action));
+    //        if (parameters != null)
+    //            cmd.Parameters.AddRange(parameters);
+
+    //        SqlDataAdapter da = new SqlDataAdapter(cmd);
+    //        da.Fill(dt);
+    //    }
+
+    //    return dt;
+    //}   
+
+    private DataTable ExecuteStoredProcedure(string action, List<SqlParameter> parameters)
+    {
+        DataTable dt = new DataTable();
+
+        using (SqlConnection con = new SqlConnection(conStr))
+        using (SqlCommand cmd = new SqlCommand("sp_LedgerMaster", con))
+        {
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@Action", action));
+            if (parameters != null)
+            {
+                foreach (var param in parameters)
+                {
+                    cmd.Parameters.Add(param);
+                }
+            }
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+        }
+
+        return dt;
+    }
+
+
+    public class LedgerModel
+    {
+        public long Ledger_Id { get; set; }
+        public string Name { get; set; }
+        public int Gender_Id { get; set; }
+        public string ContactNo { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public int State_Id { get; set; }
+        public int City_Id { get; set; }
+        public string PinCode { get; set; }
+    }
+
+
+}
---
+++ DemoWebApi1/JS/LedgerMasterJS.js
@@ -0,0 +1,118 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    loadGenders();
+    loadStates();
+    //loadLedgerList();
+    loadLedgerTable();
+    var isDrpLoaded = 0;
+
+    document.getElementById("ddlState").addEventListener("change", () => {
+        let stateId = document.getElementById("ddlState").value;
+        loadCities(stateId);
+        isDrpLoaded = 1;
+    });
+
+    let stateId = document.getElementById("ddlState").value;
+    if (stateId > 0 && isDrpLoaded == 0)
+        loadCities(stateId);
+});
+
+function loadGenders() {
+    axios.get('/api/ledgermaster/gender')
+        .then(function (response) {
+            let ddl = document.getElementById("ddlGender");
+            response.data.forEach(function (item) {
+                ddl.options.add(new Option(item.Gender_Name, item.Gender_Id));
+            });
+        })
+        .catch(function (error) {
+            console.error("Error loading genders:", error);
+        });
+}
+
+function loadStates() {
+    const ddlState = document.getElementById("ddlState");
+    ddlState.innerHTML = '<option value="">-- Select State --</option>';
+
+    axios.get('/api/ledgermaster/state')
+        .then(function (response) {
+            let ddl = document.getElementById("ddlState");
+            response.data.forEach(function (item) {
+                ddl.options.add(new Option(item.State_Name, item.State_Id));
+            });
+        })
+        .catch(function (error) {
+            console.error("Error loading states:", error);
+        });
+}
+
+function loadCities(stateId) {
+    const ddlCity = document.getElementById("ddlCity");
+    ddlCity.innerHTML = '<option value="">-- Select City --</option>';
+
+    if (stateId) {
+        axios.get(`/api/ledgermaster/city/${stateId}`)
+            .then(function (response) {
+                response.data.forEach(function (item) {
+                    ddlCity.options.add(new Option(item.City_Name, item.City_Id));
+                });
+            })
+            .catch(function (error) {
+                console.error("Error loading cities:", error);
+            });
+    }
+}
+
+function saveLedger() {
+    let obj = {
+        Name: document.getElementById("txtName").value,
+        Gender_Id: document.getElementById("ddlGender").value,
+        ContactNo: document.getElementById("txtContact").value,
+        Address1: document.getElementById("txtAddress1").value,
+        Address2: document.getElementById("txtAddress2").value,
+        State_Id: document.getElementById("ddlState").value,
+        City_Id: document.getElementById("ddlCity").value,
+        PinCode: document.getElementById("txtPinCode").value
+    };
+
+    axios.post("/api/ledgermaster/save", obj)
+        .then(() => {
+            alert("Saved successfully");
+            resetForm();
+            loadLedgerList();
+        });
+}
+
+function resetForm() {
+    document.getElementById("ledgerForm").reset();
+}
+
+function loadLedgerTable() {
+    axios.get('/api/ledgermaster/list')
+        .then(function (response) {
+            const data = response.data;
+            const tbody = document.getElementById("ledgerTableBody");
+            tbody.innerHTML = "";
+
+            data.forEach((item, index) => {
+                const tr = document.createElement("tr");
+                tr.innerHTML = `
+                    <td>${index + 1}</td>
+                    <td>${item.Name}</td>
+                    <td>${item.Gender}</td>
+                    <td>${item.ContactNo}</td>
+                    <td>${item.Address1} ${item.Address2}</td>
+                    <td>${item.City}</td>
+                    <td>${item.State}</td>
+                    <td>${item.PinCode}</td>
+                    <td>
+                        <button class="btn btn-sm btn-outline-primary" onclick="editLedger(${item.Ledger_Id})"><i class="fa fa-edit"></i></button>
+                        <button class="btn btn-sm btn-outline-danger" onclick="deleteLedger(${item.Ledger_Id})"><i class="fa fa-delete"></i></button>
+                    </td>
+                `;
+                tbody.appendChild(tr);
+            });
+        })
+        .catch(function (error) {
+            console.error("Error loading ledger data:", error);
+        });
+}
